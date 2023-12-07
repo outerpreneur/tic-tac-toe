a9dfe8f9a7c98b395dd0d81cfb360f3806a32a65
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-12-07 09:16:32 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        darkNavy: "#1A2A33",
+        semiDarkNavy: "#1F3641",
+        lightYellow: "#F2B137",
+        lightBlue: "#31C3BD",
+        silverColor: "#A8BFC9",
+      },
+    },
+  },
+  plugins: [],
+};
