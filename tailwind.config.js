1720e45dd7fed71343f964fe00d4429afa1f1751
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-12-08 09:57:31 +0100

    game settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
         lightBlue: "#31C3BD",
         silverColor: "#A8BFC9",
       },
+      fontFamily: {
+        fontOutfit: "Outfit",
+      },
+      colors: {
+        darkNavy: "#1A2A33",
+        semiDarkNavy: "#1F3641",
+        lightYellow: "#F2B137",
+        lightBlue: "#31C3BD",
+        silverColor: "#A8BFC9",
+      },
+      boxShadow: {
+        shadowInner: "-1px -8px 10px -3px rgba(0,0,0,1) inset",
+      },
     },
   },
   plugins: [],
